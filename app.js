5b3ff2e4444801f8e77c6416e93f4a7a5b15ec69
Scott Dixon <scott@howrefreshing.com.au>
2018-05-21 11:03:51 +1000

    Initial commit: Express server + routes

---
+++ app.js
@@ -0,0 +1,45 @@
+// dependencies
+const express = require('express');
+const routes = require('./routes/index');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(require('express-session')({
+  secret: 'secret123',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// development error handler
+if (app.get('env') === 'development') {
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler, no stacktraces leaked to user
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(3000, () => console.log('Listening on http://localhost:3000'));
